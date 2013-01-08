6cf6221d1abf090db9eb95b002702f54a85b8c1a
tailor <dann.croitoru@gmail.com>
2013-01-08 16:57:53 +0200

    lab 1,2

---
+++ lesson2/kmain.c
@@ -0,0 +1,71 @@
+#include "lib/video.c"
+#include "lib/io.c"
+#include "lib/kbd.c"
+
+int main( void )
+{
+    int index,i,stringSize=0;
+    char temp;
+    char input[160];
+    int lineNr= 1;
+//  putc('L');
+
+//  puts("LIDGW");
+//  puts("GOOBY PLS");
+//  clrscr();
+/*
+  for(;;){
+  input = getchar();
+  switch(input){
+   case '\n':
+      putc('r');
+      break;
+
+   default:
+      putc(input);
+      break;
+  }
+}
+*/
+  for(;;){
+  temp = getchar();
+  switch(temp){
+    case '\n':
+        lineNr++;
+      if(lineNr>24) {
+     break;
+      }
+
+      else{
+        setNewLine();
+        puts("you typed: ");
+        puts(input);
+        setNewLine();
+        for(i=0;i<stringSize;i++){
+        input[i] = ' ';
+        }
+        stringSize = 0;
+        break;
+      }
+
+
+      break;
+
+
+    case 'd':
+      clrscr();
+      for(i=0;i<stringSize;i++){
+      input[i] = ' ';
+      }
+      stringSize = 0;
+      lineNr = 1;
+      break;
+    default:
+      //putc(temp);
+
+      input[stringSize] = temp;
+      putc(input[stringSize]);
+      stringSize++;
+  }
+  }
+}
